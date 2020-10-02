da3aa4bb58232a368f11e56f381add4174dd6a2b
Nicholas Reynolds <nicholasreynolds.lambda@gmail.com>
2020-10-02 16:04:16 -0700

    Define view model and add text listener

--- app/src/main/java/com/krain/mievolauncher/MainActivity.kt
+++ app/src/main/java/com/krain/mievolauncher/MainActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 
 import com.krain.mievolauncher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        viewModel.pm = this.packageManager
         binding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_main
+        )
+        binding.command.addTextChangedListener(
+            { _,_,_,_ -> },
+            { charSequence: CharSequence?,_,_,_ ->
+                viewModel.updateSuggestions(charSequence)
+            },
+            {}
         )
     }
 
     override fun onResume() {
         super.onResume()
-
+        viewModel.refreshApps()
     }
 }
--- app/src/main/java/com/krain/mievolauncher/MainActivityViewModel.kt
+++ app/src/main/java/com/krain/mievolauncher/MainActivityViewModel.kt
@@ -1,7 +1,27 @@
 package com.krain.mievolauncher
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
+    var pm: PackageManager? = null
+    var suggestions = MutableLiveData<List<String>>()
+    private lateinit var apps : List<ApplicationInfo>
 
+    fun refreshApps() {
+        if (pm == null) {
+            return
+        }
+        apps = pm!!.getInstalledApplications(PackageManager.GET_META_DATA)
+    }
+
+    fun updateSuggestions(seq: CharSequence?) {
+        if (seq == null) {
+            return
+        }
+        // determine suggestions
+        // update recycler
+    }
 }
