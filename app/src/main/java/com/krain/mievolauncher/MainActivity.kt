c4dbae27f57726056ddb559bf8fd207a6fa35021
Nicholas Reynolds <nicholasreynolds.lambda@gmail.com>
2020-10-02 11:32:43 -0700

    Add ic resources and change theme

--- app/src/main/java/com/krain/mievolauncher/MainActivity.kt
+++ app/src/main/java/com/krain/mievolauncher/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.krain.mievolauncher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+
 import com.krain.mievolauncher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+        binding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_main
         )
+    }
 
-        val viewModel: MainActivityViewModel by viewModels()
+    override fun onResume() {
+        super.onResume()
+
     }
 }
