e0e01065ac1811978d8a4772882ca868db0470eb
Nicholas Reynolds <nicholasreynolds.lambda@gmail.com>
2020-10-18 11:51:09 -0700

    Index app table columns used in order by

--- app/src/main/java/com/krain/mievolauncher/room/model/App.kt
+++ app/src/main/java/com/krain/mievolauncher/room/model/App.kt
@@ -1,7 +1,8 @@
 package com.krain.mievolauncher.room.model
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = [Index(value = ["name", "count"])])
 data class App(@PrimaryKey var pkg: String, var name: String, var count: Int)
